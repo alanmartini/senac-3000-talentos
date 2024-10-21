77c0ba6a6ee3b5a19ef77cfb28acf6c8f54de046
Alan Martini <alanmmartini@gmail.com>
2024-10-21 18:12:11 -0300

    aula-1

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# senac-3000-talentos
+# Aula 01 - Desenvolvimento Backend
 
-O conteúdo das aulas estão nos respectivos branches
+[Acessar Slides](https://docs.google.com/presentation/d/1qdVjWc57PTmmg46ddo9sB5NilSG2eBWoVdLxtTThijI/edit?usp=sharing)
---
+++ exercicio3.js
@@ -0,0 +1,12 @@
+/*
+3) Sistema de Avaliação
+Crie uma função que determine se um aluno passou ou reprovou com base em duas notas. 
+Para passar, o aluno deve ter uma média maior ou igual a 6 e não ter faltado em mais de 10% das aulas.
+*/
+
+let nota1 = 7;
+let nota2 = 5;
+let faltas = 8;
+let totalAulas = 100;
+
+// Verifique se o aluno passou ou reprovou
